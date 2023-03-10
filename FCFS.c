d1683f1aedb6092d7fbba098fc4993e4eae676ef
Aviral <117539544+VendettaSAMURAI@users.noreply.github.com>
2023-03-10 21:02:26 +0530

    Update FCFS.c

--- FCFS.c
+++ FCFS.c
@@ -25,14 +25,13 @@ void main()
         scanf("%d", &at[i]);
     }
 
-    // Initialize the first element of temp to 0
+    
     temp[0] = 0;
 
     printf("Process\tBurst Time\tArrival Time\tWaiting Time\tTurnaround Time\tCompletion Time\n");
 
     for (i = 0; i < n; i++)
     {
-        // Calculate waiting time, turnaround time, and completion time for each process
         wt[i] = 0;
         tat[i] = 0;
         temp[i + 1] = temp[i] + bt[i];
@@ -40,15 +39,15 @@ void main()
         tat[i] = wt[i] + bt[i];
         compl [i] = temp[i + 1];
 
-        // Add waiting time and turnaround time to the total
+       
         awt += wt[i];
         atat += tat[i];
 
-        // Print the results for the current process
+        
         printf("%d\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n", i + 1, bt[i], at[i], wt[i], tat[i], compl [i]);
     }
 
-    // Calculate the average
+    
     awt /= n;
     atat /= n;
 
